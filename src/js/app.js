89829e5093450a4269cc9ce02ed4a1cc9bd1a9a1
Eric Gregoire <09egrego@gmail.com>
2017-11-30 14:50:46 -0600

    Comments to guide through container rendering

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+// Using ReactDOM that we imported which has a method to render a
+// React component ('App') into a particular element that exists in the DOM ('#app')
 ReactDOM.render(
   <App />,
   document.getElementById('app'),
